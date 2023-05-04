ccb236e3ba5ac47072ed265ee5b00defde0bc39c
Ashik-Faysal <ashikfaysal.rc@gmail.com>
2023-05-04 08:13:45 +0600

    added recipes json file

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 const categories = require('./data/categories.json');
-const blogs = require('./data/blogs.json')
+const blogs = require('./data/blogs.json');
+const foods = require('./data/foods.json');
+const recipes = require('./data/recipes.json');
 
 app.use(cors());
 
@@ -14,8 +16,21 @@ app.get('/', (req, res) => {
 app.get('/categories', (req, res) => {
     res.send(categories);
 });
+
+app.get("/categories/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedCategories = categories.find((n) => n._id === id);
+  res.send(selectedCategories);
+});
+
 app.get('/blogs', (req, res) => {
     res.send(blogs);
+})
+app.get('/foods', (req, res) => {
+    res.send(foods);
+})
+app.get('/recipes', (req, res) => {
+    res.send(recipes);
 })
 
 app.listen(port, () => {
